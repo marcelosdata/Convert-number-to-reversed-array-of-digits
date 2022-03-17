33e98205a4ac9e56bc8bb0907df50746049c75cf
Marcelo Salazar Najar <47803149+marcelosdata@users.noreply.github.com>
2022-03-17 20:34:51 +0000

    explanation on how was made

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //Convert number to reversed array of digits
 //Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+  return Array.from(String(n), Number).reverse()
+}
+// in this excersie first the numbers needed to become arrays using the array from method, then transforming them into string and the number constructor as a second argument?? then reverse
+console.log(digitize(34559))
